60058d6d008e5be9b595f3adb17a1d4f4a21135b
scott <scottjthayer@gmail.com>
2021-09-27 13:53:08 -0400

    Added favfood and removed old code

--- Program.cs
+++ Program.cs
@@ -18,8 +18,11 @@ namespace Day1Exercise
             Console.WriteLine("Can you give me your hometown?");
             string hometown = Console.ReadLine();
 
+            Console.WriteLine("Can you give me your favorite food?");
+            string food = Console.ReadLine();
+
             //display data in a nice and pretty formatted way
-            Console.WriteLine($"Your name is {name}.\nYour age is {age}.\nYour hometown is {hometown}.");
+            Console.WriteLine($"Your name is {name}.Your age is {age}.Your hometown is {hometown}. Your favorite food is {food}");
 
 
         }
